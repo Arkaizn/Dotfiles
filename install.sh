35f00fc512360651d681194e63663288bd797e87
arkaizn <silas.gubler@hispeed.ch>
2024-09-11 22:29:34 +0200

    added zsh to intall.sh

--- install.sh
+++ install.sh
@@ -14,12 +14,17 @@ sudo pacman -Syu --noconfirm
 
 # Install essential packages
 echo -e "${YELLOW}Installing essential packages...${NC}"
-sudo pacman -S --noconfirm vim zsh git neovim
+sudo pacman -S --noconfirm nano zsh oh-my-zsh fastfetch
 
 # GNOME installation
 echo -e "${YELLOW}Installing Xorg, GNOME, and GDM...${NC}"
 sudo pacman -S --noconfirm xorg xorg-server gnome gdm
 
+# Install Zsh
+echo -e "${YELLOW}Installing zsh...${NC}"
+sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 # Enable GDM service
 echo -e "${YELLOW}Starting and enabling GDM service...${NC}"
 sudo systemctl start gdm.service
@@ -27,3 +32,8 @@ sudo systemctl enable --now gdm.service
 
 echo -e "${GREEN}Installation complete! GNOME is set up and running.${NC}"
 
+# Copy Dotfiles
+cp ~/git/Dotfiles/dotfiles/.zshrc ~/
+
+# Source Zsh config
+source ~/.zshrc
