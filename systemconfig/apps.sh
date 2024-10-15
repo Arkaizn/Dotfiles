8f5f2d5cc210106a2aafd36d498989293d79a0c0
arkaizn <silas.gubler@hispeed.ch>
2024-10-15 22:44:08 +0200

    formatted package lists

--- systemconfig/apps.sh
+++ systemconfig/apps.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
+# packages list
+aur_packages=(
+    "vscodium-bin"
+    "nordvpn-bin"
+    "modrinth-app"
+)
+pacman_packages=(
+    "steam"
+    "thunderbird"
+    "pacman-contrib"
+    "gnome-terminal"
+)
+flatpaks=(
+    "com.github.tchx84.Flatseal"
+    "com.mattjakeman.ExtensionManager"
+    "io.github.zen_browser.zen"
+)
+
+
 # yay installation
 # Install necessary dependencies
-sudo pacman -S --needed git base-devel
-
+sudo pacman -S --needed base-devel
 # Clone, build, and install yay in a subshell
 (
     git clone https://aur.archlinux.org/yay.git
@@ -12,13 +30,6 @@ sudo pacman -S --needed git base-devel
 )
 rm -rf yay
 
-echo "yay installation complete!"
-# aur packages
-
-# List of AUR packages
-aur_packages=(
-    "vscodium-bin"
-)
 # Iterate over the AUR packages and install if not already installed
 for pkg in "${aur_packages[@]}"; do
     if ! yay -Q "$pkg" &> /dev/null; then
@@ -29,15 +40,6 @@ for pkg in "${aur_packages[@]}"; do
     fi
 done
 
-
-# pacman packages
-# List of Pacman packages
-pacman_packages=(
-    "steam"
-    "thunderbird"
-    "pacman-contrib"
-    "gnome-terminal"
-)
 # Iterate over the Pacman packages and install if not already installed
 for pkg in "${pacman_packages[@]}"; do
     if ! pacman -Q "$pkg" &> /dev/null; then
@@ -48,13 +50,6 @@ for pkg in "${pacman_packages[@]}"; do
     fi
 done
 
-# Flatpaks
-# List of Flatpak packages
-flatpaks=(
-    "com.github.tchx84.Flatseal"
-    "com.mattjakeman.ExtensionManager"
-    "io.github.zen_browser.zen"
-)
 # Iterate over the list and install if not already installed
 for app in "${flatpaks[@]}"; do
     if ! flatpak list | grep -q "$app"; then
@@ -64,6 +59,3 @@ for app in "${flatpaks[@]}"; do
         echo "$app is already installed. Skipping installation."
     fi
 done
-
-# Curl
-sh <(curl -sSf https://downloads.nordcdn.com/apps/linux/install.sh)
