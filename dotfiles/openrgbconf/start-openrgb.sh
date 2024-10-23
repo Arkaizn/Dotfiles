697b77998d289730383339b7a40437204e915ff6
arkaizn <silas.gubler@hispeed.ch>
2024-10-23 18:59:45 +0200

    optimized

--- dotfiles/openrgbconf/start-openrgb.sh
+++ dotfiles/openrgbconf/start-openrgb.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-sudo openrgb --startminimized --profile /root/.config/OpenRGB/purple.orp
 
+# Check if OpenRGB is running
+if ! pgrep -x "openrgb" > /dev/null
+then
+    sudo openrgb --startminimized --profile /root/.config/OpenRGB/purple.orp
+else
+    sudo systemctl stop openrgb.service
+fi
