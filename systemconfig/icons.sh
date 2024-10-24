37723247da39ae6042e7bba8006ebbce50d322c2
arkaizn <silas.gubler@hispeed.ch>
2024-10-24 22:57:37 +0200

    added icons script

---
+++ systemconfig/icons.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+(
+    curl -L -o Bibata-Modern-Ice.tar.xz https://github.com/ful1e5/Bibata_Cursor/releases/download/v2.0.7/Bibata-Modern-Ice.tar.xz
+    7z x Bibata-Modern-Ice.tar.xz
+    7z x Bibata-Modern-Ice.tar
+    cp -r Bibata-Modern-Ice ~/.icons
+)
