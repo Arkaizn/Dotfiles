f6a2a5a3058cb35c29ba4d7dcdfd3f59fd134ab2
arkaizn <silas.gubler@hispeed.ch>
2024-10-24 22:05:17 +0200

    deleted gnome parts

--- postinstall.sh
+++ postinstall.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-# install gnome themes and icons...
-echo -e "${YELLOW}Do you want to install gnome tweaks? (y/n)${NC}"
-    read -r -p "Answer: " response
-    case "$response" in
-        ""|[yY][eE][sS]|[yY])
-            sudo pacman -S gnome-tweaks --noconfirm
-            mkdir ~/.themes
-            mkdir ~/.icons
-            cp -r ./systemconfig/themes/* ~/.themes/
-            cp -r ./systemconfig/icons/* ~/.icons/
-            ;;
-        *)
-            echo -e "${YELLOW}Skipping...${NC}"
-            ;;
-    esac
-
 # setup openrgb
 echo -e "${YELLOW}Do you want to install openrgb? (y/n)${NC}"
     read -r -p "Answer: " response
@@ -34,18 +18,6 @@ echo -e "${YELLOW}Do you want to install some other apps that you probably need?
     case "$response" in
         ""|[yY][eE][sS]|[yY])
             bash ./systemconfig/apps.sh
-            ;;
-        *)
-            echo -e "${YELLOW}Skipping...${NC}"
-            ;;
-    esac
-
-# install extensions
-echo -e "${YELLOW}Do you want to install gnome extensions, is needed for the theme of gnome. (y/n)${NC}"
-    read -r -p "Answer: " response
-    case "$response" in
-        ""|[yY][eE][sS]|[yY])
-            bash ./systemconfig/gnomeextensions.sh
             ;;
         *)
             echo -e "${YELLOW}Skipping...${NC}"
