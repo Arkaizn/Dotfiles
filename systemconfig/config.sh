23448388bfba1c7e0b171e5d4716901660f8ab52
arkaizn <silas.gubler@hispeed.ch>
2024-10-25 23:03:09 +0200

    renamed file

---
+++ systemconfig/config.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# copy hypr config
+cp -r ./.config/hypr ~/.config
+
+# copy alacritty config
+cp -r ./.config/alacritty ~/.config
--- systemconfig/hypr.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# copy hypr config
-cp -r ./.config/hypr ~/.config
