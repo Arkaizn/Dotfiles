525dffa4e4a345aecf2efbdabd5dd759e862ad1a
arkaizn <silas.gubler@hispeed.ch>
2024-12-21 11:54:45 +0100

    add zsh install

--- postinstall.sh
+++ postinstall.sh
@@ -24,4 +24,18 @@ echo -e "${YELLOW}Do you want to install some other apps that you probably need?
             ;;
     esac
 
+# 3. install zsh
+echo -e "${YELLOW}When you install Zsh, it will prompt you to start using it. If you choose to do so, you can type 'exit' in Zsh to return to your previous shell, allowing the script to continue running.${NC}"
+echo -e "${YELLOW}Do you want to install zsh and configure it? (y/n)${NC}"
+read -r -p "Answer: " response
+case "$response" in
+    ""|[yY][eE][sS]|[yY])
+        echo -e "${YELLOW}Installing and configuring zsh...${NC}"
+        bash ./systemconfig/zshinstall.sh
+        ;;
+    *)
+        echo -e "${YELLOW}Skipping zsh installation.${NC}"
+        ;;
+esac
+
 # install nvidia drivers for Maxwell (NV110) series and newer
