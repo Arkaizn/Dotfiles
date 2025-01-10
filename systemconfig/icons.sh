94a19ecd849425f6fcedec97a761ab8c2b4d3c96
Silas Gubler <silas.gubler@federtechnik.ch>
2025-01-10 15:24:19 +0100

    add missing folder

--- systemconfig/icons.sh
+++ systemconfig/icons.sh
@@ -4,5 +4,6 @@
     curl -L -o Bibata-Modern-Ice.tar.xz https://github.com/ful1e5/Bibata_Cursor/releases/download/v2.0.7/Bibata-Modern-Ice.tar.xz
     7z x Bibata-Modern-Ice.tar.xz
     7z x Bibata-Modern-Ice.tar
+    mkdir ~/.icons/
     cp -r Bibata-Modern-Ice ~/.icons/Bibata-Modern-Ice
 )
