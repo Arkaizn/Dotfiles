e9c90933f810b543a725bf5bdd7116ca6d034313
Silas Gubler <silas.gubler@federtechnik.ch>
2025-01-10 10:30:02 +0100

    fix cursor

--- systemconfig/icons.sh
+++ systemconfig/icons.sh
@@ -4,5 +4,5 @@
     curl -L -o Bibata-Modern-Ice.tar.xz https://github.com/ful1e5/Bibata_Cursor/releases/download/v2.0.7/Bibata-Modern-Ice.tar.xz
     7z x Bibata-Modern-Ice.tar.xz
     7z x Bibata-Modern-Ice.tar
-    cp -r Bibata-Modern-Ice ~/.icons
+    cp -r Bibata-Modern-Ice ~/.icons/Bibata-Modern-Ice
 )
