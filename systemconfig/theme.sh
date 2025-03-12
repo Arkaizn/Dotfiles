d3e61349bc9c89785f8ca43e46790a2ee5dc1b3c
arkaizn <silas.gubler@hispeed.ch>
2025-03-12 11:51:40 +0100

    add new gtk theme and icon theme

--- systemconfig/theme.sh
+++ systemconfig/theme.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-(
-    git clone https://github.com/vinceliuice/Graphite-gtk-theme
-    cd Graphite-gtk-theme
-    ./install.sh --tweaks float colorful nord rimless -t teal
-)
-gsettings set org.gnome.desktop.interface gtk-theme 'Graphite-teal-Dark-nord'
+# (
+#    git clone https://github.com/vinceliuice/Graphite-gtk-theme
+#    cd Graphite-gtk-theme
+#    ./install.sh --tweaks float colorful nord rimless -t teal
+#)
+# gsettings set org.gnome.desktop.interface gtk-theme 'Graphite-teal-Dark-nord'
+
+cp -fr ./systemconfig/systemconfig/theme/Skeuos-Violet-Dark ~/.themes/Skeuos-Violet-Dark/
+gsettings set org.gnome.desktop.interface gtk-theme 'Skeuos-Violet-Dark'
