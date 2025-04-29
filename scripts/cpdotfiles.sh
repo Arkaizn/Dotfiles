8f9c3bdf87222a116a2b7becf7c031294e4588f5
arkaizn <silas.gubler@hispeed.ch>
2025-04-29 23:30:21 +0200

    add cpdotfiles

---
+++ scripts/cpdotfiles.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# copy hypr files to dotfiles
+cp -fr ~/.config/hypr ~/git/Dotfiles/.config/
+
+# copy pywal config to dotfiles
+cp -fr ~/.config/wal ~/git/Dotfiles/.config/
+
+# copy wofi config to dotfiles
+cp -rf ~/.config/wofi ~/git/Dotfiles/.config/
+
+# copy wlogout config to dotfiles
+cp -rf ~/.config/wlogout ~/git/Dotfiles/.config/
+
+# copy waybar config to dotfiles
+cp -rf ~/.config/waybar ~/git/Dotfiles/.config/
+
+# copy kitty config to dotfiles
+cp -rf ~/.config/kitty ~/git/Dotfiles/.config/
+
+# copy swaync config to dotfiles
+cp -rf ~/.config/swaync ~/git/Dotfiles/.config/
+
+# copy fastfetch config to dotfiles
+cp -rf ~/.config/fastfetch ~/git/Dotfiles/.config/
