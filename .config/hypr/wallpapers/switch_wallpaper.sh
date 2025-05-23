f900e3b5d81de8fffc2c31e2663ec28760bf443e
arkaizn <silas.gubler@hispeed.ch>
2025-05-23 20:15:32 +0200

    fix color picker, strange colors

--- .config/hypr/wallpapers/switch_wallpaper.sh
+++ .config/hypr/wallpapers/switch_wallpaper.sh
@@ -24,7 +24,7 @@ main() {
     
     # Generate colors with pywal
     wal --backend colorthief -i "$selected_wallpaper"
-    
+    wal -i "$selected_wallpaper"
     # Reload swaync theme
     swaync-client --reload-css
     
