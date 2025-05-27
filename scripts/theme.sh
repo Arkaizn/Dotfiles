34698a29cc0d3083d2bd9c548267e183b8fb4e86
arkaizn <silas.gubler@hispeed.ch>
2025-05-27 23:33:23 +0200

    add transparent theme for gtk

--- scripts/theme.sh
+++ scripts/theme.sh
@@ -10,3 +10,5 @@
 mkdir ~/.themes
 cp -fr ./scripts/theme/Skeuos-Violet-Dark ~/.themes/Skeuos-Violet-Dark/
 gsettings set org.gnome.desktop.interface gtk-theme 'Skeuos-Violet-Dark'
+
+yay -S materia-transparent-gtk-theme-git --noconfirm
