16ada17073974f593bc2b5c3cfb2d4fe217cd83c
arkaizn <silas.gubler@hispeed.ch>
2025-06-17 21:00:47 +0200

    add tailscale and nordvpn

--- additions/random.sh
+++ additions/random.sh
@@ -4,3 +4,18 @@
 yay -S xpadneo-dkms
 yay -S btop
 yay -S openlinkhub
+
+# tailscale
+yay -S tailscale
+sudo systemctl enable --now tailscaled.service
+sudo tailscale set --operator=$USER 
+sudo tailscale login
+tailscale set --accept-dns=false 
+tailscale status
+
+# nordvpn
+sudo groupadd nordvpn
+sudo usermod -aG nordvpn $USER
+sudo systemctl enable --now nordvpnd.service
+nordvpn login
+nordvpn login --callback "continue Button URL from Nordvpn"
